c4c9aa3b148d9c498c2ebe72a06774e07e1abe8a
Ivan Bykov <102910350+9697601836@users.noreply.github.com>
2023-01-16 18:14:02 +0300

    add Task_2+

---
+++ HW_3_JavaScript/Task_2+.js
@@ -0,0 +1,19 @@
+// Task 2*** 
+// Реализуйте считывание c JSONки из файла Task2.json с помощью, например, модуля fs. 
+// Для дальнейшего использования в функции, описанной в задании
+var fs = require('fs');
+var file_readed = fs.readFileSync("D:/json/task2.json", "utf8");
+// console.log('type file_readed = '+ typeof(file_readed))
+//
+let uniqueName = [];
+let kekw = [];
+const NewUsers = JSON.parse(file_readed).filter(item => {
+    if (!uniqueName.includes(item.name)){
+        uniqueName.push(item.name)
+        console.log(item.name)
+        return kekw.push(item)
+    } else {
+        console.log('duplicate - ', item.name)
+    }
+})
+console.log(kekw)
